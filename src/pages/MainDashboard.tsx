511923be9574312bea7cb9dcb3f701354bcc49b5
Luis <luis.diaz.navarro.210@gmail.com>
2026-08-29 02:23:56 -0600

    Import correction

--- src/pages/MainDashboard.tsx
+++ src/pages/MainDashboard.tsx
@@ -1,7 +1,7 @@
 import Navbar from '../components/layout/Navbar'
 import '../styles/MainDashboard.css'
 import '../styles/Layout.css'
-import Resume from '../components/dashboard/resume'
+import Resume from '../components/dashboard/Resume'
 
 export default function Main() {
   return (
